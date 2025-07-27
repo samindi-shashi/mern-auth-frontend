7d3d3258950bc8a1cb2061f59494be084f424280
W.W.S.S WIJESEKRA <22ug3-0241@sltc.ac.lk>
2025-07-27 18:14:38 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      target: 'https://51249a54ab10.ngrok-free.app/',
       changeOrigin: true,
       credentials: true
     })
